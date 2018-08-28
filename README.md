ff3ec29879ff4739d0e262a8edffb6e6bd04a2ae
Metehan <metehancet@gmail.com>
2018-08-28 21:37:00 +0300

    0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# turkishnlp
+# Turkish NLP
+
+Very early version of the TurkishNLP. For now it has basically 2 main functions; Detecting Turkish Language and correcting typos in Turkish words.
+
+## Dataset
+Dataset was created by parsing and filtering a Turkish wikipedia dump. 
+
+## Getting Started
+
+
+
+### Downloading the data
+
+
+
+
+### Usage
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='turkishnlp',
+    version='0.0.1',
+    packages=['detector', 're', 'pickle', 'os', 'sys', 'urllib.request', 'collections'],
+    url='github',
+    license='MIT',
+    author='Metehan Cetinkaya',
+    author_email='metehancet@gmail.com',
+    description='A python script for Turkish NLP'
+)
