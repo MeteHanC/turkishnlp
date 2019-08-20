507af76ece9a2223e756559b4ca218cb6992fc17
Metehan Çetinkaya <metehancet@gmail.com>
2019-08-20 23:51:46 +0300

    Set encoding to utf8 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.0.6",
     packages=['turkishnlp'],
     description="A python script that processes Turkish language",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding="utf8").read(),
     long_description_content_type='text/markdown',
     url="https://github.com/MeteHanC/turkishnlp",
     author="Metehan Cetinkaya",
